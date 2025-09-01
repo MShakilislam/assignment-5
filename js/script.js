18a248b027f99f21b4ecab0f01755587d6a1aaec
MShakilislam <shakilkpicse@gmail.com>
2025-09-01 21:03:30 +0600

    copy btn

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
+// hard counter section js 
+let counter = 0;
+const hardCound = document.getElementById("hard-count");
+const hardBnt = document.getElementById("hard-btn").addEventListener('click',function(){
+    counter +=1;
+    hardCound.innerText = counter
+})
 
-console.log("ekhane script jog korano hoyece")
+// text copy 
+let copyCount = 0;
+function copyText(textId){
+    const textElement = document.getElementById(textId);
+    const textCopy = textElement.innerText;
+
+    navigator.clipboard.writeText(textCopy).then(() => {
+        copyCount++;
+        document.getElementById("copyCounter").innerText =  + copyCount;
+        alert(`"${textCopy}" copid from ${textId}`);
+    }).catch(err => {
+        console.log('Service Copy',err)
+    })
+}
